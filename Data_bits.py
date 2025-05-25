2745e9d6b2b8bb03e6698b5bf9f97dda26d1e8ca
Cifrey <cifreybusiness@gmail.com>
2025-05-25 16:24:31 +0200

    Add files via upload

---
+++ Data_bits.py
@@ -0,0 +1,57 @@
+# logical operator = and, or, not
+# compound condition = 2 ó más logical operators en un if
+# comparison operator = (==, !=, <, >, <=, >=)
+# ternary operator = one-line of if-else
+
+# .append() sólo funciona con listas, no tuplas
+
+# list comprehension sólo funciona con listas (con una excepción específica)
+
+# random.choice() espera una secuencia, no funciona con diccionarios
+
+# Para asegurarme del input de usuario, es buena idea usar try-except. Ejemplo:
+while True:
+    try:
+        bet = int(input("Place your bet amount: "))
+        if bet <= 0:
+            print("Bet must be greater than 0")
+            continue
+        break
+    except ValueError:
+        print("Invalid bet")
+
+# \n para hacer una línea vacía antes o después
+
+Principles = {
+       "ETC = Easy To Change", 
+       "DRY = Don't Repeat Yourself",
+       "KISS = Keep It Simple, Stupid"
+}
+
+# Colores originales del tema: 
+# "workbench.colorCustomizations": {
+#   "editor.selectionBackground": "#264F78",
+#   "editor.selectionHighlightBackground": "#FFFFFF0B",
+#   "editor.wordHighlightBackground": "#575757B8"
+# }
+
+# dunder method = double underscore (así se llama a __)
+
+# CamelCase = UpperCamelCase
+#             lowerCamelCase (most common)
+           
+# PascalCase = capitalized, no " " or "_"
+#              ex. FirstName
+
+# snake_case = all minus, "_" instead of " " 
+
+# dot operator = attribute access operator
+
+# instanciar significa crear un objeto a partir de una clase 
+
+# @abstractmethod es un decorador, se pone encima de la función que queremos que todas las subclases 
+# hereden para que escriban su propia versión. Si no escribimos la subclase, no podemos instanciarla 
+# (es decir, convertirla en objeto). Por ejemplo, si tenemos Shapes y queremos que la subclase Circle,
+# Square, Triangle, etc, tenga su propia fórmula de calcular el área, y queremos asegurarnos que sí o sí
+# está implementada en el código, podemos convertirlo en un método abstracto y así para poder trabajar con
+# ello, hay que definir la fórmula para cada subclase obligatoriamente
